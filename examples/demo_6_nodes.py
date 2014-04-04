2cc0402d5fc0116e89322e5f93e911db7e4f7bb7
Logan Zhou <xorcererzc@gmail.com>
2014-04-04 16:55:58 +0800

    Updated demo.

--- examples/demo_6_nodes.py
+++ examples/demo_6_nodes.py
@@ -4,13 +4,13 @@ from models.space import Space, Node, Vector2D
 
 if __name__ == '__main__':
     space = Space(size=(600, 600), no_relevance_distance=100,
-                  step_factor=0.0001)
+                  step_factor=0.0003)
 
     center = Node(0, Vector2D(100, 100), color=(1.0, 1.0, 0.5))
     a = Node(1, Vector2D(100, 110))
     b = Node(2, Vector2D(200, 120))
-    c = Node(3, Vector2D(300, 130))
-    d = Node(4, Vector2D(400, 140))
+    c = Node(3, Vector2D(300, 133))
+    d = Node(4, Vector2D(400, 133))
     e = Node(5, Vector2D(500, 150))
 
     value = 1.0
